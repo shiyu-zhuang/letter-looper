79b8e2751d98d77aaff30b93d69148cd4550a3ba
Shiyu Zhuang <zhua0008@algonquinlive.com>
2017-01-23 13:30:09 -0500

    Update looper.js

--- js/looper.js
+++ js/looper.js
@@ -1,11 +1,8 @@
 /*global console, arrays, echo, prompt*/
-
 var letter, i, k;
-
 while (!letter) {
     letter = prompt("Enter a character.");
 }
-
 for (i = 0; i < 10; i += 1) {
     for (k = 0; k <= i; k += 1) {
         document.write(letter);
